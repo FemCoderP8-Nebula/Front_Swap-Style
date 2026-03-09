4d8497b1845098e9af0d15fb93e38400a318eebf
Leonela88 <rivasaela@gmail.com>
2026-03-09 18:44:32 +0100

    head resolved

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 import LoginForm from "../../components/organisms/LoginForm/LoginForm";
-=======
-const Login=()=>{
-    return(
-        <main role="main">
-            <p>Página de Inicio de Login</p>
-            <div>
->>>>>>> a957614b4ede0d1d4376fd68ed0eebf355f125b8
 
 
 const Login = () => {
