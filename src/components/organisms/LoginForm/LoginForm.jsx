8aae3ea39794d8ffce3e0dce99d2426426e477d2
Leonela88 <rivasaela@gmail.com>
2026-03-09 19:50:20 +0100

    chore: form hooks and styles details

--- src/components/organisms/LoginForm/LoginForm.jsx
+++ src/components/organisms/LoginForm/LoginForm.jsx
@@ -1,37 +1,64 @@
-//Formulario de logeo
-
+import { useState } from "react"; 
 import styles from "./login-form.module.css";
-
 import FormField from '../../molecules/FormField/FormField';
 import Button from "../../atoms/Button/Button";
 
-
 const LoginForm = () => {
+ 
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("sended:", formData);
+    // LLamada a la API ??
+  };
+
   return (
     <section className={styles.container}>
       <h1 className={styles.title}>Login</h1>
 
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.fields}>
           <FormField
             label="Email"
+            name="email" 
             type="email"
             placeholder="Enter your email"
+            value={formData.email}
+            onChange={handleChange}
           />
           <FormField
             label="Password"
+            name="password"
             type="password"
             placeholder="Enter your password"
+            value={formData.password}
+            onChange={handleChange}
           />
         </div>
 
         <div className={styles.actions}>
+          
           <Button
+            type="submit" 
             text="Log In"
             BtnClass="neon"
-            path="/login"
           />
           <Button
+            type="button"
             text="Cancel"
             BtnClass="cancel"
             path="/home"
@@ -43,3 +70,4 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
+
